5d81f5ab10d889e1c8e692e9d38d7bf9b3ff9f57
ben.akiva <ben.akiva@passportcard.co.il>
2024-01-14 15:32:46 +0200

    worked on the main

--- src/app/todos/components/main/main.component.ts
+++ src/app/todos/components/main/main.component.ts
@@ -1,12 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodosService } from '../../services/todos.service';
 
 @Component({
-  selector: 'todomvc-main',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'todomvc-main',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainComponent {}
+export class MainComponent {
+    todosService = inject(TodosService)
+}
