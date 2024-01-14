a332f19cf4f9f627150831f1cd661cfdc0e81e92
ben.akiva <ben.akiva@passportcard.co.il>
2024-01-14 15:12:04 +0200

    create todo service

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
 
 @Component({
   selector: 'todomvc-todos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,HeaderComponent,FooterComponent,MainComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
