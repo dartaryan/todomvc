f139a26730a10857b8fd229bc07cf03169ec0fe5
ben.akiva <ben.akiva@passportcard.co.il>
2024-01-14 14:54:11 +0200

    create all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { TodosComponent } from './todos/todos.component';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'todomvc-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    standalone: true,
+    imports: [TodosComponent, RouterModule],
+    selector: 'todomvc-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'todomvc';
+    title = 'todomvc';
 }
