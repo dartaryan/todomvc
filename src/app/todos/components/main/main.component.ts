1972e3e1bec4e966f30fe72067782f1f38eaa159
ben.akiva <ben.akiva@passportcard.co.il>
2024-01-14 17:57:59 +0200

    implementing complete all

--- src/app/todos/components/main/main.component.ts
+++ src/app/todos/components/main/main.component.ts
@@ -2,7 +2,7 @@ import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/cor
 import {CommonModule} from '@angular/common';
 import {TodosService} from '../../services/todos.service';
 import {FilterEnum} from '../../types/filter.enum';
-import {TodoComponent} from "../todo/todo.component";
+import {TodoComponent} from '../todo/todo.component';
 
 @Component({
     selector: 'todomvc-main',
@@ -14,21 +14,28 @@ import {TodoComponent} from "../todo/todo.component";
 })
 export class MainComponent {
     todosService = inject(TodosService);
-    editingId: string | null = null
+    editingId: string | null = null;
     visibleTodos = computed(() => {
         const todos = this.todosService.todosSig();
         const filter = this.todosService.filterSig();
 
         if (filter === FilterEnum.active) {
-            return todos.filter(todo => !todo.isCompleted)
+            return todos.filter(todo => !todo.isCompleted);
         } else if (filter === FilterEnum.completed) {
-            return todos.filter(todo => todo.isCompleted)
+            return todos.filter(todo => todo.isCompleted);
         }
-        return todos
-    })
+        return todos;
+    });
+    isAllTodoSelected = computed(() => this.todosService.todosSig().every(todo => todo.isCompleted));
+    noTodosClass = computed(() => this.todosService.todosSig().length === 0)
+
+    toggleAllTodos(event: Event): void {
+        const target = event.target as HTMLInputElement;
+        this.todosService.toggleAll(target.checked)
+    }
 
     setEditingId(editingId: string | null): void {
-        this.editingId = editingId
+        this.editingId = editingId;
     }
 
 }
--- src/app/todos/services/todos.service.ts
+++ src/app/todos/services/todos.service.ts
@@ -21,18 +21,22 @@ export class TodosService {
     }
 
     changeTodo(id: string, text: string): void {
-        this.todosSig.update(todos =>
-            todos.map(todo => todo.id === id ? {...todo, text} : todo));
+        this.todosSig.update(todos => todos.map(todo => todo.id === id ? {...todo, text} : todo));
     }
 
     removeTodo(id: string): void {
-        this.todosSig.update(todos =>
-            todos.filter(todo => todo.id !== id));
+        this.todosSig.update(todos => todos.filter(todo => todo.id !== id));
     }
 
     toggleTodo(id: string): void {
-        this.todosSig.update(todos =>
-            todos.map(todo => todo.id === id ? {...todo, isCompleted: !todo.isCompleted} : todo));
+        this.todosSig.update(todos => todos.map(todo => todo.id === id ? {
+            ...todo,
+            isCompleted: !todo.isCompleted
+        } : todo));
     }
 
+    toggleAll(isCompleted: boolean): void {
+        this.todosSig.update(todos =>
+            todos.map(todo => ({...todo, isCompleted: !todo.isCompleted})));
+    }
 }
